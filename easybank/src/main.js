5f9ccad85e91bc08a3d34bc17679a43f435d474e
bunee <buneeisslo@gmail.com>
2023-09-13 20:05:55 +0530

    Feat: mobile nav menu(draft)

--- easybank/src/main.js
+++ easybank/src/main.js
@@ -1,3 +1,14 @@
 import "./scss/main.scss";
 
+const navToggle = document.querySelector(".nav-toggle");
+const hamburgerIcon = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".close");
+const navMenu = document.querySelector(".nav-list");
+
+navToggle.addEventListener("click", (e) => {
+    hamburgerIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    navMenu.classList.toggle("show-nav");
+})
+
 console.log("bunee!");
