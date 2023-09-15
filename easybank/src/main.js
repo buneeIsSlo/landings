0b9285e40122f703d03e01acd763bc7ef84777f4
bunee <buneeisslo@gmail.com>
2023-09-15 16:21:09 +0530

    Add: nav menu background overlay

--- easybank/src/main.js
+++ easybank/src/main.js
@@ -4,11 +4,13 @@ const navToggle = document.querySelector(".nav-toggle");
 const hamburgerIcon = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close");
 const navMenu = document.querySelector(".nav-list");
+const backgroundOverlay = document.querySelector(".bg-overlay");
 
 navToggle.addEventListener("click", (e) => {
     hamburgerIcon.classList.toggle("hidden");
     closeIcon.classList.toggle("hidden");
     navMenu.classList.toggle("show-nav");
+    backgroundOverlay.classList.toggle("hidden");
 })
 
 console.log("bunee!");
