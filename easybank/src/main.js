2746cf21a0eed6e04bdb1113a38dc7e9806aa81c
bunee <buneeisslo@gmail.com>
2023-09-18 14:28:36 +0530

    Add: lenis smooth scroll(testing)

--- easybank/src/main.js
+++ easybank/src/main.js
@@ -12,6 +12,22 @@ navToggle.addEventListener("click", (e) => {
     navMenu.classList.toggle("show-nav");
     backgroundOverlay.classList.toggle("hidden");
     navToggle.setAttribute("aria-expanded", !backgroundOverlay.classList.contains("hidden"));
+});
+
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
 })
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 console.log("bunee!");
